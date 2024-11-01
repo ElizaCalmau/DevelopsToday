448e888aa0fb2678c66a0add4258d9450cac44cf
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-01 13:48:40 +0200

    added styles for diabled button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -4,7 +4,16 @@ export default function Button({ text, isDisabled }) {
   console.log(isDisabled);
   return (
     <Link href="/select">
-      <button disabled={isDisabled}>{text}</button>
+      <button
+        className={`mt-4 px-4 py-2 rounded-lg text-white font-semibold transition duration-200 
+      ${
+        isDisabled
+          ? 'bg-gray-400 cursor-not-allowed'
+          : 'bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300'
+      }`}
+      >
+        {text}
+      </button>
     </Link>
   );
 }
