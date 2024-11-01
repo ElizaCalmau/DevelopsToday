3e19c52716b5aa29c54edca1cd5e6fdf48b390c9
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-01 12:19:38 +0200

    add Loading component

---
+++ src/app/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="loader border-t-transparent justify-center border-4 border-blue-600 rounded-full w-16 h-16 animate-spin"></div>
+    </div>
+  );
+}
